218f87792dccbd99ca1816ccd654338578504e1b
pragyashahi12 <pragyashahi1202@gmail.com>
2026-04-10 12:28:50 +0530

    Update practiceset.py

--- practiceset.py
+++ practiceset.py
@@ -1,6 +1,6 @@
-# #1. Print numbers 1 to 10
-# for i in range(1,11):
-#  print(i)
+#1. Print numbers 1 to 10
+for i in range(1,11):
+  print(i)
 
 # #2.Print even numbers (1-20)
 # print("The list of even number is:")
